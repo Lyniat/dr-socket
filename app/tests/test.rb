f3e554ced254360922f1168e06eacb2d932d4874
Laurin Muth <l.muth@lyniat.games>
2023-09-14 13:15:36 +0200

    Cleanup code

--- app/tests/test.rb
+++ app/tests/test.rb
@@ -1,7 +1,7 @@
 # run test: ./dragonruby dr-socket --eval app/tests/test.rb --no-tick
 
-require "app/tests/serialize.rb"
+#require "app/tests/serialize.rb"
 
-puts $socket.get_build_info
-puts "running tests"
-$gtk.tests.start
+#puts $socket.get_build_info
+#puts "running tests"
+#$gtk.tests.start
