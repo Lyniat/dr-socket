45dd0e91c9bac50e1fd1c5962f37352916570be8
Laurin Muth <l.muth@lyniat.games>
2023-09-15 14:29:00 +0200

    Minor change to unit test

--- app/tests/test.rb
+++ app/tests/test.rb
@@ -1,7 +1,5 @@
 # run test: ./dragonruby dr-socket --eval app/tests/test.rb --no-tick
 
-#require "app/tests/serialize.rb"
-
-#puts $socket.get_build_info
-#puts "running tests"
-#$gtk.tests.start
+puts $socket.get_build_info
+puts "running tests"
+$gtk.tests.start
